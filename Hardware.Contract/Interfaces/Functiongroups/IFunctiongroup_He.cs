13dc580e43ffda3afe8c58eba4e4915a241cdef3
Marvin Buß <marvin.buss@schiller.de>
2024-01-21 15:58:31 +0100

    Refactor

--- Hardware.Contract/Interfaces/Functiongroups/IFunctiongroup_He.cs
+++ Hardware.Contract/Interfaces/Functiongroups/IFunctiongroup_He.cs
@@ -3,16 +3,20 @@ using Hardware.Contract.Types;
 
 namespace Hardware.Contract.Interfaces.Functiongroups;
 
-public interface IFunctiongroup_HE_
+public interface IFunctiongroup_He
 {
     ISensor BG11 { get; }
+
     ISensor BG21 { get; }
     IEngine MA01 { get; }
+
 
     public bool EngineIsTemporaryLocked { get; set; }
     public DriveState CurrentState { get; }
 
     public Position CurrentPosition { get; }
+    event Action BG11IsOccupied;
+    event Action BG21IsOccupied;
     void OpenShutter();
     void CloseShutter();
     void StopDriving();
